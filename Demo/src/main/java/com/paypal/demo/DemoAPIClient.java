31767a46401200f2edc268821fb43463b562cc48
scannillo <samantha.cannillo@getbraintree.com>
2020-06-25 06:36:24 -0500

    Update sample merchant server to fetch from /id-token

--- Demo/src/main/java/com/paypal/demo/DemoAPIClient.java
+++ Demo/src/main/java/com/paypal/demo/DemoAPIClient.java
@@ -12,7 +12,7 @@ import retrofit2.http.Query;
 
 public interface DemoAPIClient {
 
-    @GET("/client-token")
+    @GET("/id-token")
     Call<PayPalUAT> getPayPalUAT(@Query("countryCode") String countryCode);
 
     @POST("/order")
